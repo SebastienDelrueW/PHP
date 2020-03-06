cbea0de9c2595d126e901b736b42ed3431069f1b
SebastienDelrueW <58477200+SebastienDelrueW@users.noreply.github.com>
2020-03-06 17:01:48 +0100

    workshop

---
+++ 00_workshop/exo.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Exo workshop</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> 
+</head>
+<body>
+    <div class="jumbotron">
+        <h1>Exo Workshop</h1>
+        <hr>
+        <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam porro ad assumenda voluptatum aspernatur delectus deserunt blanditiis praesentium, similique consequatur vero? Maxime, qui. Facilis, blanditiis? Alias at id quasi praesentium.
+        </p>
+    </div>
+
+    <section class="container bg-warning">
+    <div class="row">
+        <div class="col-md-12">
+        <form method="post" action="">
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="prenom">Prénom</label>
+      <input type="text" class="form-control" id="prenom">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="nom">Nom</label>
+      <input type="nom" class="form-control" id="nom">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+            ici mettre le form
+
+
+        </div> <!--fin col-->
+    </div> <!--fin row -->
+    <div class="row">
+        <div class="col-md-12">
+         <table class="table table-sm table-dark">
+  <thead>
+    <tr>
+      <th scope="col">id contact</th>
+      <th scope="col">prénom</th>
+      <th scope="col">nom</th>
+      <th scope="col">email</th>
+      <th scope="col">adresse</th>
+      <th scope="col">telephone</th>
+      <th scope="col">mot de passe</th>
+      <th scope="col">notes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>55 rue de la Pierre</td>
+      <td>06 05 08 08 08</td>
+      <td>650gdqqg05</td>
+      <td>production et finances</td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    
+    </div>
+
+</p>  section 
+ <!-- Optional JavaScript en CDN -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS en CDN-->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
+</body>
+</html>
