dc2b7190f1d03a3f04aa46be31649554934c8b42
SebastienDelrueW <58477200+SebastienDelrueW@users.noreply.github.com>
2020-03-11 09:19:50 +0100

    Update dialogue.php

--- 07_SECURITE/dialogue.php
+++ 07_SECURITE/dialogue.php
@@ -13,4 +13,22 @@
                               pseudo                VARCHAR 
                               message               TEXT
                               date_enregistrement   DATETIME
+
+
 */
+
+// 1. Formulaire HTML
+
+?>
+<h1>Votre message</h1>
+<form method="post" action="">
+    <label for="pseudo"></label>
+    <input type="text" name="pseudo" id="pseudo" value="">
+    <br>
+
+    <label for="message">Message</label>
+    <textarea name="message" id="messsage"></textarea>
+    <br>
+
+    <input type="submit">
+</form>
