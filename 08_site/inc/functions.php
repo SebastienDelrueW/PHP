6f7517282857f4d1a6da3c79d4685f119c1839ae
SebastienDelrueW <58477200+SebastienDelrueW@users.noreply.github.com>
2020-03-12 16:57:10 +0100

    Connexion

--- 08_site/inc/functions.php
+++ 08_site/inc/functions.php
@@ -32,3 +32,25 @@ function estAdmin(){
     
     }
 }
+
+// Fonction pour exécuter  toutes les requêtes préparées :
+
+function executeRequete($requete, $parametres = array()){
+
+    // Assainissement des données avec htmlspecialchars : on parle d'échapper les données (échappement) :
+    foreach($parametres as $indice => $valeur){
+                $parametres[$indice] = htmlspecialchars($valeur);
+    } // on parcourt le tableau $parametres qui contient les marqueurs et leur valeur. On prend chaque valeur que l'on passe dans htmlspecialchars() pour transformer les chevrons en entité HTML. Cette valeur une fois assainie, on la remet dans son emplacement qui est $parametres[$indice].
+
+    global $pdo; // global permet d'accéder à la variable $pdo qui est définie dans l'espace global du fichier init.php.
+
+    $resultat = $pdo->prepare($requete); // on prépare la requête qui est contenu dans la variable de la requête.
+    $succes = $resultat->execute($parametres); // puis on l'exécute en donnant le tableau $parametres qui associe les marqueurs à leur valeur. execute() retourne true si la requête à marché sinon false, et on affecte ce résultat à la variable succes.
+
+    if($succes === false){
+        return false; // si la requête n'a pas marché, on retourne false.
+    }else{
+        return $resultat; // en cas de succès on retourne l'objet PDOStatement qui contient le jeu de résultats.
+    }
+
+}
